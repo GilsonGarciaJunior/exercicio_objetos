de246a3986ed50474eac6f07820f59b7b9fdd3ab
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-05-17 09:43:01 -0300

    melhorando o js

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-function Carro(nome, modelo, anoFrabricado, anoModelo, velocidadeMax) {
+function veiculos(nome, modelo) {  
     this.nome = nome;
     this.modelo = modelo;
-    this.anoFrabricado = anoFrabricado;
-    this.anoModelo = anoModelo;
-    this.velocidadeMax = velocidadeMax;
 }
 
-const carroAlex = new Carro("Lancia", "Stratos HF ", 1973, 1974, "230 km/h");
-const carroHenrique = new Carro("Audi", "Quattro", 1980, 1983, "220 km/h");
+function Moto (nome, modelo, anoFabricado, anoModelo){
+    this.nome = nome;
+    this.modelo = modelo;
+    this.anoFabricado = anoFabricado;
+    this.anoModelo = anoModelo;
+
+    veiculos.call();
+}
+
+function Carro (nome, modelo, anoFabricado, anoModelo){
+    this.nome = nome;
+    this.modelo = modelo;
+    this.anoFabricado = anoFabricado;
+    this.anoModelo = anoModelo;
+    
+    veiculos.call();
+}
+
+const carroAlex = new Carro("Lancia", "Stratos HF ", "1973", "1974");
+const carroHenrique = new Carro("Audi", "Quattro", "1980", "1983");
+const motoMaria = new Moto("Honda", "CG 160", "2015", "2026");
+const motoJuan = new Moto("Yamaha", "Fazer", "2005", "2006");
+
+console.log(carroAlex);
+console.log(carroHenrique);
+console.log(motoJuan);
+console.log(motoMaria);
