168ea388941f02cc917b99380463b01a1035bc2c
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-05-19 20:20:52 -0300

    changed JS

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-function veiculos(nome, modelo) {  
-    this.nome = nome;
-    this.modelo = modelo;
-}
-
-function Moto (nome, modelo, anoFabricado, anoModelo){
-    this.nome = nome;
-    this.modelo = modelo;
-    this.anoFabricado = anoFabricado;
-    this.anoModelo = anoModelo;
-
-    veiculos.call();
-}
-
-function Carro (nome, modelo, anoFabricado, anoModelo){
-    this.nome = nome;
-    this.modelo = modelo;
-    this.anoFabricado = anoFabricado;
-    this.anoModelo = anoModelo;
+class garagem{
+    constructor(nome, modelo, anoFabricado, anoModelo){
+        this.nome = nome;
+        this.modelo = modelo;
+        this.anoFabricado = anoFabricado;
+        this.anoModelo = anoModelo;
+    }
     
-    veiculos.call();
+    exibir() {
+        console.log(`Nome: ${this.nome}, Modelo: ${this.modelo}, Ano de Fabricação: ${this.anoFabricado} ,Ano do modelo: ${this.anoModelo}`);
+    }
+}
+
+class Moto extends garagem{
+    constructor (nome, modelo, anoFabricado, anoModelo){
+        super(nome, modelo, anoFabricado, anoModelo);
+    }
+
+    exibir() {
+        super.exibir();
+    }
+}
+
+class Carro extends garagem{
+    constructor (nome, modelo, anoFabricado, anoModelo){
+        super(nome, modelo, anoFabricado, anoModelo);
+    }
+    
+    exibir() {
+        super.exibir();
+    }
 }
 
 const carroAlex = new Carro("Lancia", "Stratos HF ", "1973", "1974");
@@ -26,7 +36,7 @@ const carroHenrique = new Carro("Audi", "Quattro", "1980", "1983");
 const motoMaria = new Moto("Honda", "CG 160", "2015", "2026");
 const motoJuan = new Moto("Yamaha", "Fazer", "2005", "2006");
 
-console.log(carroAlex);
-console.log(carroHenrique);
-console.log(motoJuan);
-console.log(motoMaria);
+carroAlex.exibir();
+carroHenrique.exibir();
+motoJuan.exibir();
+motoMaria.exibir();
